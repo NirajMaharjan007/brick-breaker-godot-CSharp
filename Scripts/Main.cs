00f646898f1355828e0da0e9c98cf63ee5fdb853
NirajMhrjn@007 <niraj.mhrjn770@gmail.com>
2025-09-09 12:27:08 +0545

    Minor Changed
    Added Check Wall of Camera

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -4,14 +4,23 @@ namespace MyGame.Scripts;
 
 public partial class Main : Node2D
 {
+    private Paddle paddle;
+
+    private Camera2D camera2D;
+
     public override void _Ready()
     {
         base._Ready();
+
+        paddle = GetNode<Paddle>("Paddle");
+        camera2D = GetNode<Camera2D>("Camera2D");
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
+
+        paddle.CheckWall(camera2D);
     }
 
     public override void _PhysicsProcess(double delta)
--- Scripts/Paddle.cs
+++ Scripts/Paddle.cs
@@ -4,18 +4,57 @@ namespace MyGame.Scripts;
 
 public partial class Paddle : CharacterBody2D
 {
-	public override void _Ready()
-	{
-		base._Ready();
-	}
+    private const int SPEED = 200;
 
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		base._PhysicsProcess(delta);
-	}
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        Move();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+
+    private void Move()
+    {
+        var velocity = Vector2.Zero;
+        if (Input.IsActionPressed("ui_left"))
+        {
+            velocity.X = -SPEED;
+        }
+        else if (Input.IsActionPressed("ui_right"))
+        {
+            velocity.X = SPEED;
+        }
+        else
+            velocity.X = Mathf.MoveToward(velocity.X, 0, SPEED);
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+    public void CheckWall(Camera2D camera)
+    {
+        Vector2 pos = GlobalPosition;
+
+        float left = camera.LimitLeft + 16;
+        float right = camera.LimitRight - 50;
+
+        // Clamp to camera limits
+        float clampedX = Mathf.Clamp(pos.X, left, right);
+        float clampedY = Mathf.Clamp(pos.Y, camera.LimitTop, camera.LimitBottom);
+
+        var str =
+            $"camera limitd left and right {left} and {right} respectively\n Paddle->{GlobalPosition}";
+        GD.Print(str);
+
+        GlobalPosition = new Vector2(clampedX, clampedY);
+    }
 }
