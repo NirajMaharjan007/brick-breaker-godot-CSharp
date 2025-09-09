32a2ea4c83b51affc612ed0b7c56a0e548a242a8
NirajMhrjn@007 <niraj.mhrjn770@gmail.com>
2025-09-09 10:34:13 +0545

    ADDED

---
+++ Scripts/Main.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+namespace MyGame.Scripts;
+
+public partial class Main : Node2D
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+}
