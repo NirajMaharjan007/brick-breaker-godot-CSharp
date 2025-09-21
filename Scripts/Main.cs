d14d35c17cfa6e8e52787103e323e1908ca04a65
NirajMhrjn@007 <niraj.mhrjn770@gmail.com>
2025-09-21 13:18:17 +0545

    Minor FIXEs and UPDATEs
    little FIXES

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -10,6 +10,8 @@ public partial class Main : Node2D
 
     private Ball ball;
 
+    private Node2D bricksContainer;
+
     public override void _Ready()
     {
         base._Ready();
@@ -19,6 +21,39 @@ public partial class Main : Node2D
         paddle = GetNode<Paddle>("Paddle");
 
         ball = GetNode<Ball>("Ball");
+
+        bricksContainer = GetNode<Node2D>("BricksContainer");
+
+        var shape =
+            bricksContainer
+                .GetNode<Area2D>("Area2D")
+                .GetNode<CollisionShape2D>("CollisionShape2D")
+                .Shape as RectangleShape2D;
+
+        var areaSize = shape.Size;
+
+        var brickScene = GD.Load<PackedScene>("res://Scenes/Brick.tscn");
+
+        var tempBrick = brickScene.Instantiate<Brick>();
+        float brickWidth = tempBrick.Width;
+        float brickHeight = tempBrick.Height;
+        tempBrick.QueueFree();
+
+        int col = (int)(areaSize.X / brickWidth);
+        int row = (int)(areaSize.Y / brickHeight);
+
+        GD.Print($"Brick area size: {areaSize}");
+        GD.Print($"Rows: {row}, Cols: {col}");
+
+        for (int y = 0; y < row; y++)
+        {
+            for (int x = 0; x < col; x++)
+            {
+                var brick = brickScene.Instantiate<Brick>();
+                brick.Position = new Vector2(x * brick.Width, y * brick.Height);
+                bricksContainer.AddChild(brick);
+            }
+        }
     }
 
     public override void _Process(double delta)
