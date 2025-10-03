3b60b64c5a8e6112a6a7d19d14f42d535a6f66bf
NirajMhrjn@007 <niraj.mhrjn770@gmail.com>
2025-10-03 22:57:18 +0545

    Minor CHANGES

--- Scripts/Loading.cs
+++ Scripts/Loading.cs
@@ -7,6 +7,8 @@ public partial class Loading : Node
     private VBoxContainer container;
     private ProgressBar progressBar;
 
+    private double targetValue = 0;
+
     public override void _Ready()
     {
         base._Ready();
@@ -14,35 +16,26 @@ public partial class Loading : Node
 
         container = GetNode<VBoxContainer>("VBoxContainer");
         progressBar = container.GetNode<ProgressBar>("ProgressBar");
-
-        var timer = GetNode<Timer>("Timer");
-        timer.Timeout += On_Timer_timeout;
-        timer.Start();
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
 
-        progressBar.Value += 10;
-    }
+        targetValue += 100;
+        progressBar.Value = Mathf.MoveToward(
+            (float)progressBar.Value,
+            (float)targetValue,
+            (float)(32 * delta) // speed of fill
+        );
 
-    private void On_Timer_timeout()
-    {
-        try
+        GD.Print($"Progress: {progressBar.Value}/{progressBar.MaxValue}");
+        GD.Print($"Target: {targetValue}");
+
+        if (progressBar.Value >= progressBar.MaxValue)
         {
-            if (progressBar.Value >= progressBar.MaxValue)
-            {
-                GD.Print("Loading complete!");
-                GetTree().ChangeSceneToFile("res://Scenes/Game/Main.tscn");
-                QueueFree();
-            }
-        }
-        catch (System.Exception e)
-        {
-            GD.PushError($"Failed to change scene: {e}");
-            GD.PrintErr($"Error during loading: {e.Message}\n{e.StackTrace}");
-            System.Environment.Exit(-1);
+            GetTree().ChangeSceneToFile("res://Scenes/Game/Main.tscn");
+            QueueFree();
         }
     }
 }
