c3253a1944a27f9939cc596c57de8bb1e359698b
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2025-10-05 18:40:46 +0545

    Minor Update

--- Scripts/Loading.cs
+++ Scripts/Loading.cs
@@ -22,11 +22,11 @@ public partial class Loading : Node
     {
         base._Process(delta);
 
-        targetValue += 100;
+        targetValue += 16;
         progressBar.Value = Mathf.MoveToward(
             (float)progressBar.Value,
             (float)targetValue,
-            (float)(32 * delta) // speed of fill
+            (float)(64 * delta) // speed of fill
         );
 
         GD.Print($"Progress: {progressBar.Value}/{progressBar.MaxValue}");
