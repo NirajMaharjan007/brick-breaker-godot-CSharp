1d9c334a6d2edfa18ee5cf5e7c41fb546f396723
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2025-11-08 17:00:12 +0545

    added

---
+++ Scripts/Heart.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+namespace MyGame.Scripts;
+
+public partial class Heart : AnimatedSprite2D
+{
+    private float width;
+    private float height;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        var area = GetNode<Area2D>("Area2D");
+        var shape = area.GetNode<CollisionShape2D>("CollisionShape2D").Shape as RectangleShape2D;
+        width = shape?.Size.X ?? 0f;
+        height = shape?.Size.Y ?? 0f;
+    }
+
+    public float Width => width;
+    public float Height => height;
+}
